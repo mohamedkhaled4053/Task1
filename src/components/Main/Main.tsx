07927b4470c78e8122707b6658ecd786bff77efc
Mohamed Khaled <mohamedkhaled4053@gmail.com>
2023-09-28 20:09:20 +0300

    info card style

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -2,8 +2,9 @@ import "./style.scss";
 import Navbar from "../Navbar/Navbar";
 // import { ApplicationForm, mockData } from "../utils/mock";
 // import { useState } from "react";
-import { Form, Upload } from "antd";
+import { Form, Upload, Checkbox, Switch } from "antd";
 import { ImgIcon } from "../Icon/ImgIcon";
+import { Icon } from "../Icon/Icon";
 
 const Main = () => {
   //   let [FormData, setFormData] = useState<ApplicationForm>(mockData);
@@ -34,6 +35,58 @@ const Main = () => {
                 </p>
               </Upload.Dragger>
             </Form.Item>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card-header">Upload cover image</div>
+          <div className="card-body">
+            <div className="infos">
+              <div className="info">
+                <p className="title">First Name</p>
+                <Form.Item
+                  name={["personalInformation", "firstName", "internal"]}
+                >
+                  <Checkbox>Internal</Checkbox>
+                </Form.Item>
+                <Form.Item name={["personalInformation", "firstName", "show"]}>
+                  <Switch id="test" /> <label htmlFor="test">Hide</label>
+                </Form.Item>
+              </div>
+              <div className="info">
+                <p className="title">First Name</p>
+                <Form.Item
+                  name={["personalInformation", "firstName", "internal"]}
+                >
+                  <Checkbox>Internal</Checkbox>
+                </Form.Item>
+                <Form.Item name={["personalInformation", "firstName", "show"]}>
+                  <Switch id="test" /> <label htmlFor="test">Hide</label>
+                </Form.Item>
+              </div>
+              <div className="info">
+                <p className="title">First Name</p>
+                <Form.Item
+                  name={["personalInformation", "firstName", "internal"]}
+                >
+                  <Checkbox>Internal</Checkbox>
+                </Form.Item>
+                <Form.Item name={["personalInformation", "firstName", "show"]}>
+                  <Switch id="test" /> <label htmlFor="test">Hide</label>
+                </Form.Item>
+              </div>
+            </div>
+            <div className="add-question">
+              <Icon name="add" width={25} /> Add a question
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card-header">Upload cover image</div>
+          <div className="card-body">
+            <div className="question saved">
+              <p className="title">Please tell me about yourself in less than 500 words</p>
+              <ImgIcon name="edit" width={18}/>
+            </div>
           </div>
         </div>
       </Form>
