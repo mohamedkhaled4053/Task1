76043e8c3eb1c4e8b87abbc5066151f5f24e882e
Mohamed Khaled <mohamedkhaled4053@gmail.com>
2023-09-28 10:30:40 +0300

    setup side bar

---
+++ src/components/Side/Side.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./style.scss";
+import { Icon } from "../Icon/Icon";
+import { ImgIcon } from "../Icon/ImgIcon";
+
+const Side = () => {
+  return (
+    <div className="side">
+      <Icon name="menu" />
+      <ImgIcon name="home" className="home"/>
+      <ImgIcon name="note" className="note"/>
+      <div className="name">
+        NT
+      </div>
+    </div>
+  );
+};
+
+export default Side;
