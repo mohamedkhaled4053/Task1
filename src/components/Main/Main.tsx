a4d10ea993cda7773c1efbdbc1590a4b7da8abb1
Mohamed Khaled <mohamedkhaled4053@gmail.com>
2023-09-28 13:21:12 +0300

    put the navbar in component

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,33 +1,10 @@
-import { useState } from "react";
 import "./style.scss";
+import Navbar from "../Navbar/Navbar";
 
 const Main = () => {
-  let [activeNavTitle, setActiveNavTitle] =
-    useState<string>("Application Form");
-
-  let navTitles = [
-    "Program Details",
-    "Application Form",
-    "Workflow",
-    "Preview",
-  ];
-
   return (
     <div className="main">
-      <div className="nav">
-        <div className="nav-content">
-          {navTitles.map((title) => (
-            <div
-              className={`nav-title ${
-                title === activeNavTitle ? "active" : ""
-              }`}
-              onClick={()=>setActiveNavTitle(title)}
-            >
-              {title}
-            </div>
-          ))}
-        </div>
-      </div>
+      <Navbar />
     </div>
   );
 };
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import "./style.scss";
+
+const Navbar = () => {
+  let [activeNavTitle, setActiveNavTitle] =
+    useState<string>("Application Form");
+
+  let navTitles = [
+    "Program Details",
+    "Application Form",
+    "Workflow",
+    "Preview",
+  ];
+
+  return (
+    <div className="nav">
+      <div className="nav-content">
+        {navTitles.map((title) => (
+          <div
+            className={`nav-title ${title === activeNavTitle ? "active" : ""}`}
+            onClick={() => setActiveNavTitle(title)}
+          >
+            {title}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
