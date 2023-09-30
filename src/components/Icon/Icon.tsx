2b60fe72f9b1d1a02120eed6ef711e9dc6b61a8d
Mohamed Khaled <mohamedkhaled4053@gmail.com>
2023-09-30 11:32:08 +0300

    use state instead of ref in Icon

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 interface IconProps extends React.SVGProps<SVGSVGElement> {
   name: string;
@@ -6,18 +6,20 @@ interface IconProps extends React.SVGProps<SVGSVGElement> {
 
 // dynamically import svg by the name
 export const Icon: React.FC<IconProps> = ({ name, ...rest }) => {
-  const ImportedIconRef = useRef<React.FC<React.SVGProps<SVGSVGElement>>>();
+  const [importedIcon, setImportedIcon] =
+    useState<React.FC<React.SVGProps<SVGSVGElement>>>();
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     setLoading(true);
     const importIcon = async (): Promise<void> => {
       try {
-        ImportedIconRef.current = (
+        let importedIcon = (
           await import(
             `!!@svgr/webpack?-svgo,+titleProp,+ref!../../assets/svgs/${name}.svg`
           )
         ).default;
+        setImportedIcon(importedIcon);
       } catch (err) {
         // error handling
       } finally {
@@ -27,8 +29,8 @@ export const Icon: React.FC<IconProps> = ({ name, ...rest }) => {
     importIcon();
   }, [name]);
 
-  if (!loading && ImportedIconRef.current) {
-    let SvgIcon = ImportedIconRef.current;
+  if (!loading && importedIcon) {
+    let SvgIcon = importedIcon;
     return <SvgIcon {...rest} />;
   }
   return null;
